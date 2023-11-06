da63c456f16a0d90748fcf7d617590b177011dcf
gustavocsb <gcsb03@hotmail.com>
2023-11-06 09:49:22 -0300

    Atualizando WikipediaScraper.py
    Limpando a falta de dados na coluna 'Notas' adicionando uma string vazia.
    Gerando um arquivo .xlsx com os dados filtrados da tabela de filósofos.

--- WikipediaScraper.py
+++ WikipediaScraper.py
@@ -2,22 +2,39 @@ from bs4 import BeautifulSoup
 import requests
 
 url = 'https://pt.wikipedia.org/wiki/Lista_de_fil%C3%B3sofos_da_Gr%C3%A9cia_Antiga'
-page = requests.get(url)
-soup = BeautifulSoup(page.text, 'html')
+pagina = requests.get(url)
+soup = BeautifulSoup(pagina.text, 'html.parser')
 
-# <table class= "wikitable sortable jquery-tablesorter">
+# Class da tabela descoberta inspecionando a página: <table class= "wikitable sortable jquery-tablesorter">
 
-# Pegando todas as tabelas e achando o dado que será extraído com índice
-table = soup.find_all('table')[1]
+# Pegando todas as tabelas e achando a tabela onde estão os dados que serão extraídos, usando índice
+tabela = soup.find_all('table')[1]
 
 # Meio alternativo, pegando a tabela com a class dos dados que serão extraídos
-# print(soup.find('table', class_ = 'wikitable sortable'))
+# soup.find('table', class_ = 'wikitable sortable')
 
-titles = soup.find_all('th')
+titulos = tabela.find_all('th')
 
-print(titles)
+tabela_titulos = [titulo.text.strip() for titulo in titulos]
 
-table_titles = [title.text.strip() for title in titles]
+import pandas as pd
 
-print(table_titles)
+df = pd.DataFrame(columns = tabela_titulos)
 
+# Os dados dentro desta table estão dentros de tr(rows) e td(data)
+
+coluna_dados = tabela.find_all('tr')
+
+for linha in coluna_dados[1:]:
+    linha_dados = linha.find_all('td')
+    individual_linha_dados = [data.text.strip() for data in linha_dados]
+    # Corrigindo 2 linhas onde não estava puxando a última coluna 'Notas', onde estava sem dados no site
+    while len(individual_linha_dados) < len(df.columns):
+        individual_linha_dados.append('')
+
+    tamanho = len(df)
+    df.loc[tamanho] = individual_linha_dados
+
+print(df)
+
+df.to_excel(r'Filosofos.xlsx', index=False)
